208771d780b5d09252caf8b7ebeab0949810bfd0
DavidGDA <davidgabriel261006@gmail.com>
2024-09-07 23:12:08 -0600

    v1.0.1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const path = require('path')
-const port = 3000;
+const path = require("path");
+const port = process.env.PORT || 4000;
 
 const app = express();
 
