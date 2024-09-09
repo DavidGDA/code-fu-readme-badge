e95fe1299ca636ec13858ab3dad35bda8cd30726
DavidGDA <davidgabriel261006@gmail.com>
2024-09-09 00:49:35 -0600

    stash migrate changes to new branch

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const path = require("path");
-const port = process.env.PORT || 4000;
+const port = 3000;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
     res.redirect("/badges/badge.svg");
