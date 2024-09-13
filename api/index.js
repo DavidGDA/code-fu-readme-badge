16c3bc304b35c7015cd76db8975f8ba901f52f3c
DavidGDA <davidgabriel261006@gmail.com>
2024-09-12 20:59:23 -0600

    :tada: añadiendo badges de staff

--- api/index.js
+++ api/index.js
@@ -6,36 +6,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("/badges/default-badge", (req, res) => {
-  res.redirect("/badges/default-badge.svg");
-});
-
-app.get("/badges/cruiz", (req, res) => {
-  res.redirect("/badges/cruiz.svg");
-});
-
-app.get("/badges/dalvarez", (req, res) => {
-  res.redirect("/badges/dalvarez.svg");
-});
-
-app.get("/badges/areynosa", (req, res) => {
-  res.redirect("/badges/areynosa.svg");
-});
-
-app.get("/badges/jsolis", (req, res) => {
-  res.redirect("/badges/jsolis.svg");
-});
-
-app.get("/badges/ralfaro", (req, res) => {
-  res.redirect("/badges/ralfaro.svg");
-});
-
-app.get("/badges/afonseca", (req, res) => {
-  res.redirect("/badges/afonseca.svg");
-});
-
-app.get("/badges/arodriguez", (req, res) => {
-  res.redirect("/badges/arodriguez.svg");
+app.get("/", (req, res) => {
+  res.send("CDN is on the way");
 });
 
 app.listen(port, () => {
