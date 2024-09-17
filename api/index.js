4ef7e90d823ec3d24ffb975bf9ec85791e2eed1c
DavidGDA <davidgabriel261006@gmail.com>
2024-09-16 19:48:26 -0600

    Fetch de los datos desde la API de scrapp

--- api/index.js
+++ api/index.js
@@ -15,44 +15,14 @@ app.get("/", (req, res) => {
 app.get("/badges/:staffName", async (req, res) => {
   const staffName = req.params.staffName;
   try {
-    const url = "https://code-fu.net.ni/staff";
-    const browser = await puppeteer.launch({
-      headless: true,
-    });
-    const page = await browser.newPage();
-    await page.goto(url);
-
-    const staffFullName = await page.$eval(`#${staffName} h2`, (element) => {
-      return element.textContent;
-    });
-
-    const staffCargo = await page.$eval(`#${staffName} span`, (element) => {
-      return element.textContent;
-    });
-
-    let staffImageUrl = await page.$eval(`#${staffName} img`, (element) => {
-      return element.outerHTML.match(/src="([^"]*)/)[1];
-    });
-
-    if (staffImageUrl.startsWith("data:image")) {
-      staffImageUrl = await page.$eval(`#${staffName} img`, (element) => {
-        return element.outerHTML.match(/data-src="([^"]*)/)[1];
+    fetch(`https://code-fu-scrapp-staff-data.onrender.com/badges/${staffName}`)
+      .then((response) => response.json())
+      .then((data) => {
+        res.json(data);
       });
-    }
-
-    const staffInfo = {
-      staffFullName: staffFullName,
-      staffCargo: staffCargo,
-      staffImageUrl: staffImageUrl,
-    };
-
-    await browser.close();
-
-    await fs.writeFile("api/images.json", JSON.stringify(staffInfo, null, 2));
   } catch (error) {
     console.error(error);
   }
-  res.sendFile(path.join(__dirname, "images.json"));
 });
 
 app.listen(port, () => {
